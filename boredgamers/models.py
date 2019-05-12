f4e1d05abde1202a264371c25a449eb6c1d690c7
adolorosa <anaesthesia.dolorosa@gmail.com>
2019-05-12 13:52:50 +0200

    Add files needed to import BGG .csv data into the database

--- boredgamers/models.py
+++ boredgamers/models.py
@@ -16,3 +16,29 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rank = db.Column(db.Integer, unique=True, nullable=False)
+    bgg_url = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    min_players = db.Column(db.Integer)
+    max_players = db.Column(db.Integer)
+    average_time = db.Column(db.Integer)
+    min_time = db.Column(db.Integer)
+    max_time = db.Column(db.Integer)
+    year = db.Column(db.Integer)
+    average_rating = db.Column(db.Float)
+    geek_rating = db.Column(db.Float)
+    num_votes = db.Column(db.Integer)
+    image_url = db.Column(db.String, unique=True)
+    age = db.Column(db.Integer)
+    mechanic = db.Column(db.String)
+    owned = db.Column(db.Integer)
+    category = db.Column(db.String)
+    designer = db.Column(db.String)
+
+    def __repr__(self):
+        return f"Game: {self.name}"
