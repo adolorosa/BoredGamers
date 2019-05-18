39ad9f94288859baadc1bdb4e109b2fb27273594
adolorosa <anaesthesia.dolorosa@gmail.com>
2019-05-18 20:01:02 +0200

    Separate the Update Account form from the Account page

--- boredgamers/routes.py
+++ boredgamers/routes.py
@@ -68,9 +68,18 @@ def save_picture(form_picture):
     return picture_fn
 
 
-@app.route("/account", methods=["GET", "POST"])
+@app.route("/account")
 @login_required
 def account():
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template(
+        "account.html", title="Account", image_file=image_file
+    )
+
+
+@app.route("/update-account/<int:user_id>", methods=["GET", "POST"])
+@login_required
+def update_account(user_id):
     form = UpdateAccountForm()
     if form.validate_on_submit():
         if form.picture.data:
@@ -81,12 +90,11 @@ def account():
         db.session.commit()
         flash("Your account has been updated!", "success")
         return redirect(url_for("account"))
-    elif request.method == "GET":
+    elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
     return render_template(
-        "account.html", title="Account", image_file=image_file, form=form
+        "update_account.html", title="Update account", user_id=user_id, form=form
     )
 
 
