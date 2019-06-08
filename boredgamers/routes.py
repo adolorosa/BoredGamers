f6c7626f88c2ea791827fd68395bcea487989609
adolorosa <anaesthesia.dolorosa@gmail.com>
2019-06-08 18:55:53 +0200

    Store profile picture in binary format

--- boredgamers/routes.py
+++ boredgamers/routes.py
@@ -57,22 +57,20 @@ def logout():
     return redirect(url_for("home"))
 
 
+
 def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
-    output_size = (125, 125)
+    output_size = (200, 200)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
-    i.save(picture_path)
-    return picture_fn
+    i_binary = i.tobytes()
+    return i_binary
+    
 
 
 @app.route("/account")
 @login_required
 def account():
-    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    image_file = Image.frombytes('RGB', (200, 200), current_user.image_file)
     return render_template(
         "account.html", title="Account", image_file=image_file
     )
@@ -85,7 +83,9 @@ def update_account(user_id):
     if form.validate_on_submit():
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
-            current_user.image_file = picture_file
+        else:
+            picture_file = save_picture(url_for("static", filename="profile_pics/default.jpg"))
+        current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         current_user.location = form.location.data
